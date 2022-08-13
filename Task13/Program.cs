e6ef9dd10921afa8eacfbdbdde47a66ba7d16efa
AsiyaKazanbasova <sarykyz88@gmail.com>
2022-08-14 00:16:54 +0600

    Added answer for Task №13

---
+++ Task13/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 13: Напишите программу, которая выводит 
+// третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+string number = Console.ReadLine();
+
+Console.WriteLine($"The third digit is: {number[2]}");
+Console.WriteLine("There is no third digit");

f6d0c7a0bd81f746f89a0587203972f7bd2f5567
AsiyaKazanbasova <sarykyz88@gmail.com>
2022-08-13 22:30:13 +0600

    Added answer of Task №10

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// Нужно сделать через числа, без индексов строк.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Enter any of three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = (number / 10) - (number / 100) * 10;
+Console.WriteLine($"The second digit of {number} is {result}");

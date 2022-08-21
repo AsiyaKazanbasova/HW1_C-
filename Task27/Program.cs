a55ea7bcb36482560fd421940897d4450ef7c691
AsiyaKazanbasova <sarykyz88@gmail.com>
2022-08-21 19:24:42 +0600

    Added answer for Task №27

---
+++ Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int GetDigitsSum(int num)
+{
+    int sum = 0;
+    int i = 0;
+    while (number > 0)
+    {
+        number /=10;
+        sum += number;
+        i++;
+    }
+    return sum;
+}
+Console.WriteLine($"Digits sum of {number} = {GetDigitsSum(number)}");

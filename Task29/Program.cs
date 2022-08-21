8f94efada96e935ea541ad7b104e14207a0b7180
AsiyaKazanbasova <sarykyz88@gmail.com>
2022-08-21 20:12:00 +0600

    Added answer for Task №29

---
+++ Task29/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив 
+// из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// Way № 1
+// int [] array = new int[8];
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(100);
+//     Console.WriteLine($"{array[i]}");
+// }
+
+// Way № 2
+
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(100);
+    }
+    return result;
+}
+
+int[] array = GetArray(8);
+
+Console.WriteLine($"Array of 8 random elements: [ {String.Join(", " ,array )} ]");

726ff3d107fe667acf74cd29dd5281f765ebb7fb
AsiyaKazanbasova <sarykyz88@gmail.com>
2022-08-24 15:18:33 +0600

    Added answer of Task №38

---
+++ Task38/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] array = new double[5];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().NextDouble() * 100;
+    Console.Write(Math.Round(array[i], 2) + " ");
+}
+double min = array[0];
+double max = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < min) min = array[i];
+    else if (array[i] > max) max = array[i];
+}
+double diff = max - min;
+Console.WriteLine($"Difference between maximum and minimum elements is: {diff}");
